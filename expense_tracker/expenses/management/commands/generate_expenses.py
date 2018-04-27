6a74630c101b6cfc11e1d9fcade255d71a0111a4
6r1d <vic.6r1d+github_messages@gmail.com>
2018-04-27 12:05:10 +0300

    Updates gitignore and example record generation script

--- expense_tracker/expenses/management/commands/generate_expenses.py
+++ expense_tracker/expenses/management/commands/generate_expenses.py
@@ -4,7 +4,9 @@ import urllib.request
 from django.core.management.base import BaseCommand, CommandError
 from expenses.models import Expense
 
-DICT_URL = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+DICT_URL = ("http://svnweb.freebsd.org/csrg/share/dict/words?"
+            "view=co&"
+            "content-type=text/plain")
 
 class Command(BaseCommand):
     help = 'Generates a number of random expenses for the sake of testing'
@@ -21,20 +23,11 @@ class Command(BaseCommand):
             amount = random.randint(3, 1450)
             date = datetime.date.today()
             time = datetime.time()
-            e = Expense(
+            expense_instance = Expense(
                 description=description, amount=amount,
-                date=date, time=time, comment=''
+                date=date, time=time, comment='',
+                user=1
             )
-            e.save()
+            expense_instance.save()
 
-        # for poll_id in options['poll_id']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-        #
-        #     poll.opened = False
-        #     poll.save()
-        #
-        #     self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
         self.stdout.write('abc')

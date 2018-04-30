2697d2c47a2b714d5fd94c868a550a551cf53784
Eugene Hatsko <e.chatzko@wis.eu.com>
2018-04-30 19:49:00 +0300

    Filter was changed by using date lookup

--- expense_tracker/expenses/filters.py
+++ expense_tracker/expenses/filters.py
@@ -9,10 +9,10 @@ class ExpensesFilter(django_filters.FilterSet):
                                        label='Search string')
     amount_from = django_filters.NumberFilter(name='amount', lookup_expr='gte')
     amount_to = django_filters.NumberFilter(name='amount', lookup_expr='lte')
-    created_from = django_filters.DateTimeFilter(name='created_at',
-                                                 lookup_expr='gte')
-    created_to = django_filters.DateTimeFilter(name='created_at',
-                                               lookup_expr='lte')
+    created_from = django_filters.DateFilter(name='created_at',
+                                             lookup_expr='date__gte')
+    created_to = django_filters.DateFilter(name='created_at',
+                                           lookup_expr='date__lte')
     user = django_filters.NumberFilter(name='user', lookup_expr='exact')
 
     def filter_by_text(self, queryset, name, value):
